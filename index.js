689e84bb930776b7725b9b032f371a251ae79274
Peter Chen <haichen@chapman.edu>
2020-01-15 13:12:24 -0800

    make it into functions

--- index.js
+++ index.js
@@ -13,230 +13,268 @@ AWS.config.getCredentials(function (err) {
 var sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 // //create a topic
-// var createTopicPromise = sns.createTopic({Name: "TestTopic2"}).promise();
+function createTopics(topicname)
+{
+    var createTopicPromise = sns.createTopic({Name: topicname}).promise();
 
-// // Handle promise's fulfilled/rejected states
-// createTopicPromise.then(
-//     function (data) {
-//         console.log("Topic's ARN: ", data.TopicArn);
-//     }).catch(
-//         function (err) {
-//             console.error(err, err.stack);
-//         }
-// );
+    // Handle promise's fulfilled/rejected states
+    createTopicPromise.then(
+        function (data) {
+            console.log("Topic's ARN: ", data.TopicArn);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //list topics
-// var listTopicsPromise = sns.listTopics({}).promise();
-// var arr = []
+function listAllTopics()
+{
+    var listTopicsPromise = sns.listTopics({}).promise();
+    var arr = []
 
-// // Handle promise's fulfilled/rejected states
-// listTopicsPromise.then(
-//     function (data) 
-//     {
-//         arr = data.Topics
-//         console.log(arr);
+    // Handle promise's fulfilled/rejected states
+    listTopicsPromise.then(
+        function (data) 
+        {
+            arr = data.Topics
+            console.log(arr);
 
-//         //print elements in the array
-//         // arr.forEach(element => {
-//         //     console.log(element)
-//         // })
-//     }).catch(
-//         function (err) {
-//             console.error(err, err.stack);
-//         }
-//     );
+            //print elements in the array
+            // arr.forEach(element => {
+            //     console.log(element)
+            // })
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 // //delete a topic
-// var deleteTopicPromise = sns.deleteTopic({ TopicArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic1' }).promise();
+function deleteTopics(arn)
+{
+    //'arn:aws:sns:us-east-1:171645004892:TestTopic1'
+    var deleteTopicPromise = sns.deleteTopic({ TopicArn: arn }).promise();
 
-// // Handle promise's fulfilled/rejected states
-// deleteTopicPromise.then(
-//     function (data) 
-//     {
-//         console.log("Topic Deleted");
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    deleteTopicPromise.then(
+        function (data) {
+            console.log("Topic Deleted");
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
-// //get topic attributes
-// var getTopicAttribsPromise = sns.getTopicAttributes({ TopicArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic2' }).promise();
+//get topic attributes
+function getTopicAttr(arn)
+{
+    //'arn:aws:sns:us-east-1:171645004892:TestTopic2'
+    var getTopicAttribsPromise = sns.getTopicAttributes({ TopicArn: arn }).promise();
 
-// // Handle promise's fulfilled/rejected states
-// getTopicAttribsPromise.then(
-//     function (data) 
-//     {
-//         console.log(data);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    getTopicAttribsPromise.then(
+        function (data) {
+            console.log(data);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
-// //setting topic attributes
+//setting topic attributes
+function setTopicAttr(attrName, arn, attrValue)
+{
+    var params = {
+        AttributeName: attrName, /* required */ //'DisplayName'
+        TopicArn: arn, /* required */ //'arn:aws:sns:us-east-1:171645004892:TestTopic2'
+        AttributeValue: attrValue //'testtopic2'
+    };
 
-// // Create setTopicAttributes parameters
-// var params = {
-//     AttributeName: 'DisplayName', /* required */
-//     TopicArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic2', /* required */
-//     AttributeValue: 'testtopic2'
-// };
+    var setTopicAttribsPromise = sns.setTopicAttributes(params).promise();
 
-// var setTopicAttribsPromise = sns.setTopicAttributes(params).promise();
-
-// // Handle promise's fulfilled/rejected states
-// setTopicAttribsPromise.then(
-//     function (data) 
-//     {
-//         console.log(data);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
-
+    // Handle promise's fulfilled/rejected states
+    setTopicAttribsPromise.then(
+        function (data) {
+            console.log(data);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //Listing Subscriptions to a Topic
-// const params = {
-//     TopicArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic2'
-// }
+function listSubscription(arn)
+{
+    const params = {
+        //'arn:aws:sns:us-east-1:171645004892:TestTopic2'
+        TopicArn: arn
+    }
 
-// var subslistPromise = sns.listSubscriptionsByTopic(params).promise();
+    var subslistPromise = sns.listSubscriptionsByTopic(params).promise();
 
-// // Handle promise's fulfilled/rejected states
-// subslistPromise.then(
-//     function (data) 
-//     {
-//         console.log(data);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    subslistPromise.then(
+        function (data) {
+            console.log(data);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 
 //Subscribing an Email Address to a Topic
+function subscribeEmail(arn, endpoint)
+{
+    // Create subscribe/email parameters
+    var params2 = {
+        Protocol: 'EMAIL', /* required */
+        TopicArn: arn, /* required */ //'arn:aws:sns:us-east-1:171645004892:TestTopic2'
+        Endpoint: endpoint //'meiguo1969@gmail.com'
+    };
 
-// // Create subscribe/email parameters
-// var params2 = {
-//     Protocol: 'EMAIL', /* required */
-//     TopicArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic2', /* required */
-//     Endpoint: 'meiguo1969@gmail.com'
-// };
+    var subscribePromise = sns.subscribe(params2).promise();
 
-// var subscribePromise = sns.subscribe(params2).promise();
-
-// // Handle promise's fulfilled/rejected states
-// subscribePromise.then(
-//     function (data) 
-//     {
-//         console.log("Subscription ARN is " + data.SubscriptionArn);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    subscribePromise.then(
+        function (data) {
+            console.log("Subscription ARN is " + data.SubscriptionArn);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //Subscribing an Application Endpoint to a Topic
+function subscribeApplication(arn,endpoint)
+{
+    // Create subscribe/email parameters
+    var params = {
+        Protocol: 'application', /* required */
+        TopicArn: arn, /* required */
+        Endpoint: endpoint
+    };
 
-// // Load the AWS SDK for Node.js
-// var AWS = require('aws-sdk');
-// // Set region
-// AWS.config.update({ region: 'REGION' });
+    // Create promise and SNS service object
+    var subscribePromise = new AWS.SNS({ apiVersion: '2010-03-31' }).subscribe(params).promise();
 
-// // Create subscribe/email parameters
-// var params = {
-//     Protocol: 'application', /* required */
-//     TopicArn: 'TOPIC_ARN', /* required */
-//     Endpoint: 'MOBILE_ENDPOINT_ARN'
-// };
-
-// // Create promise and SNS service object
-// var subscribePromise = new AWS.SNS({ apiVersion: '2010-03-31' }).subscribe(params).promise();
-
-// // Handle promise's fulfilled/rejected states
-// subscribePromise.then(
-//     function (data) {
-//         console.log("Subscription ARN is " + data.SubscriptionArn);
-//     }).catch(
-//         function (err) {
-//             console.error(err, err.stack);
-//         });
-
+    // Handle promise's fulfilled/rejected states
+    subscribePromise.then(
+        function (data) {
+            console.log("Subscription ARN is " + data.SubscriptionArn);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            });
+}
 
 //Unsubscribing from a Topic
-// var unsubscribePromise = sns.unsubscribe({ SubscriptionArn: 'arn:aws:sns:us-east-1:171645004892:TestTopic2:4ccfccf1-376d-41b3-b809-5a390edc2e28' }).promise();
+function unSubscribe(subArn)
+{
+    var unsubscribePromise = sns.unsubscribe({ SubscriptionArn: subArn }).promise();
 
-// // Handle promise's fulfilled/rejected states
-// unsubscribePromise.then(
-//     function (data) 
-//     {
-//         console.log(data);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    unsubscribePromise.then(
+        function (data) {
+            console.log(data);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //Getting SMS Attributes
-// var params3 = {
-//     attributes: [
-//         'DefaultSMSType',
-//     ]
-// };
+function getSMSAttr()
+{
+    var params3 = {
+        attributes: [
+            'DefaultSMSType',
+        ]
+    };
 
-// var getSMSTypePromise = sns.getSMSAttributes(params3).promise();
+    var getSMSTypePromise = sns.getSMSAttributes(params3).promise();
 
-// // Handle promise's fulfilled/rejected states
-// getSMSTypePromise.then(
-//     function (data) 
-//     {
-//         console.log(data);
-//     }).catch(
-//         function (err) 
-//         {
-//             console.error(err, err.stack);
-//         }
-//     );
+    // Handle promise's fulfilled/rejected states
+    getSMSTypePromise.then(
+        function (data) {
+            console.log(data);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //Checking If a Phone Number Has Opted Out
-var phonenumPromise = sns.checkIfPhoneNumberIsOptedOut({ phoneNumber: '6265608207' }).promise();
+function checkPhoneOptedOut(phoneNum)
+{
+    var phonenumPromise = sns.checkIfPhoneNumberIsOptedOut({ phoneNumber: phoneNum }).promise();
 
-// Handle promise's fulfilled/rejected states
-phonenumPromise.then(
-    function (data) 
-    {
-        console.log("Phone Opt Out is " + data.isOptedOut);
-    }).catch(
-        function (err) 
-        {
-            console.error(err, err.stack);
-        }
-    );
+    // Handle promise's fulfilled/rejected states
+    phonenumPromise.then(
+        function (data) {
+            console.log("Phone Opt Out is " + data.isOptedOut);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
+
+//Subscribing an phoen number Address to a Topic
+function subscribePhoneToTopic(arn,endpoint)
+{
+    // Create subscribe/email parameters
+    var params5 = {
+        Protocol: 'SMS', /* required */
+        TopicArn: arn, /* required */ //'arn:aws:sns:us-east-1:171645004892:TestTopic2'
+        Endpoint: endpoint //'+16265608207'
+    };
+
+    var subscribePromise = sns.subscribe(params5).promise();
+
+    // Handle promise's fulfilled/rejected states
+    subscribePromise.then(
+        function (data) {
+            console.log("Subscription ARN is " + data.SubscriptionArn);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+}
 
 //Publishing an SMS Message
-var params4 = {
-    Message: 'Hello World', /* required */
-    PhoneNumber: '+10016265608207',
-};
+function publishSMS()
+{
+    var params4 = {
+        Message: '  Hello World', /* required */
+        // PhoneNumber: '+10016265608207',
+        Subject: 'Message from Peter',
+        TargetArn: 'arn:aws:sns:us-east-1:171645004892:Phone',
+    };
 
-var publishTextPromise = sns.publish(params4).promise();
+    var publishTextPromise = sns.publish(params4).promise();
 
-// Handle promise's fulfilled/rejected states
-publishTextPromise.then(
-    function (data) 
-    {
-        console.log("MessageID is " + data.MessageId);
-    }).catch(
-        function (err) 
-        {
-            console.error(err, err.stack);
-        }
-    );
+    // Handle promise's fulfilled/rejected states
+    publishTextPromise.then(
+        function (data) {
+            console.log("MessageID is " + data.MessageId);
+        }).catch(
+            function (err) {
+                console.error(err, err.stack);
+            }
+        );
+
+}
+
+publishSMS()
